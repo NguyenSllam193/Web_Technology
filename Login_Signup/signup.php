81b46cfc4978a726ed5e5175cdf5737ca128ccbe
Son Lam <sonllam1903@gmail.com>
2021-10-24 07:50:09 +0700

    add file config, connect signup-database

--- Login_Signup/signup.php
+++ Login_Signup/signup.php
@@ -11,7 +11,7 @@ include "../header_file.php";
               <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
 
                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-                  <form class="mx-1 mx-md-4">
+                  <form class="mx-1 mx-md-4" method="post" class="form">
                     <div class="d-flex flex-row align-items-center mb-4">
                       <i class="fas fa-user fa-lg me-3 fa-fw"></i>
                       <div class="form-outline flex-fill mb-0">
