e488b34c672a459dfb2c4b8b62f2b677117e6e3e
Your Name <you@example.com>
2021-10-28 21:00:56 +0700

    test

--- login/login.php
+++ login/login.php
@@ -2,6 +2,32 @@
 include "../header_file.php";
 ?>
 
+
+<?php
+  require('../config/config.php');
+//   session_start();
+    if (isset($_POST['username'])){
+
+    $username = stripslashes($_REQUEST['user_name']);
+    $username = mysqli_real_escape_string($conn,$username);
+    $password = stripslashes($_REQUEST['user_pass']);
+    $password = mysqli_real_escape_string($conn,$password);
+        $query = "SELECT * FROM `tbl_user` WHERE user_name='$username' and user_pass='".md5($password)."'";
+    $result = mysqli_query($conn,$query) or die(mysql_error());
+    $rows = mysqli_num_rows($result);
+        if($rows==1){
+      $_SESSION['user_name'] = $username;
+      header("Location: index.php");
+            }else{
+        echo "<div class='form'><h3>Tên đăng nhập hoặc mật khẩu không đúng!</h3></br><a href='login.php'>Đăng nhập lại</a></div>";
+        }
+    }else
+?>
+
+
+
+
+
     <div class="container-fluid vh-100">
         <div class="" style="margin-top:200px">
             <div class="rounded d-flex justify-content-center">
@@ -14,12 +40,12 @@ include "../header_file.php";
                             <div class="input-group mb-3">
                                 <span class="input-group-text bg-primary"><i
                                         class="bi bi-person-plus-fill text-white"></i></span>
-                                <input type="text" class="form-control" placeholder="Username">
+                                <input type="text" name="user_name" class="form-control" placeholder="Tên Đăng Nhập">
                             </div>
                             <div class="input-group mb-3">
                                 <span class="input-group-text bg-primary"><i
                                         class="bi bi-key-fill text-white"></i></span>
-                                <input type="password" class="form-control" placeholder="password">
+                                <input type="password"  name="user_pass" class="form-control" placeholder="password">
                             </div>
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
