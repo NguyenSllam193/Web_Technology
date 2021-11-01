e430f0f70f65aeed521059292940ed29a060b403
Your Name <you@example.com>
2021-11-01 22:51:37 +0700

    update

--- .history/config/config_20211024080338.php
+++ .history/config/config_20211024080338.php
@@ -2,7 +2,7 @@
 $db_host = "127.0.0.1";
 $db_user = "root";
 $db_passwd = "";
-$db_name = "web";
+$db_name = "web_technology";
 $db_gate = "80";
 
 $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
--- login/check/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start(); //Dịch vụ bảo vệ
-    if(isset($_SESSION['loginOK'])){
-        unset($_SESSION['loginOK']);
-        header("Location: ../login.php");
-    }
-?>
