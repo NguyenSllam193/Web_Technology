c76a675f37aeb98c56175017b90168a880e1f657
beekuny1st <1851171340@e.tlu.edu.vn>
2021-11-02 22:03:52 +0700

    nam

--- member/sign_up/config.php
+++ member/sign_up/config.php
@@ -1,8 +1,9 @@
 <?php
-$db_host = "localhost";
+$db_host = "127.0.0.1";
 $db_user = "root";
 $db_passwd = "";
 $db_name = "web";
+$db_gate = "80";
 
 $conn = mysqli_connect($db_host, $db_user, $db_passwd, $db_name);
 if($conn){
---
+++ sign_in_process.php
@@ -0,0 +1,33 @@
+<?php
+include "./config/config.php";
+
+if(isset($_POST['btnLogin']) && $_POST["txtAccount"] != '' && $_POST["txtPasswd"] != ''){
+    $txtAccount = $_POST['txtAccount'];
+    $txtPasswd = $_POST['txtPasswd'];
+    
+    $sql = "SELECT * FROM admins WHERE ad_name = '$txtAccount' AND ad_pass = '$txtPasswd'";
+    $query = mysqli_query($conn, $sql);
+    $total = mysqli_fetch_assoc($query);
+    $ad_id = $total['ad_id'];
+
+    if($total > 0){
+        header("Location: ./admin/admin_page.php?ad_id=$ad_id");
+        mysqli_close($conn);
+    }else{
+        header("Location: index.php");
+    }
+
+    $sql = "SELECT * FROM users WHERE us_name = '$txtAccount' AND us_pass = '$txtPasswd'";
+    $query = mysqli_query($conn, $sql);
+    $total = mysqli_fetch_assoc($query);
+    $us_id = $total['us_id'];
+    if($total > 0){
+        header("Location: ./member/home_page/home_page.php?us_id=$us_id");
+        mysqli_close($conn);
+    }else{
+        header("Location: index.php");
+    }
+}else{
+    header("Location: index.php");
+}
+?>
